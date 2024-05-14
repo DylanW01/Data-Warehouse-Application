2e0580a76746a540636dde5eb32685cce3104051
Dylan Warrell <124311289+DylanW01@users.noreply.github.com>
2024-05-14 14:18:21 +0100

    PopularBooksByMonth Query

--- Server/src/swaggerConfig/5. ChiefLibrarian.js
+++ Server/src/swaggerConfig/5. ChiefLibrarian.js
@@ -1,40 +1,88 @@
 /**
  * @swagger
+ * components:
+ *   schemas:
+ *     PopularBooksByMonthResponse:
+ *       type: object
+ *       properties:
+ *         TITLE:
+ *           type: string
+ *         AUTHOR:
+ *           type: string
+ *         NUMBER_OF_LOANS:
+ *           type: integer
+ *         NUMBER_OF_FINES:
+ *           type: integer
+ *       example:
+ *         - TITLE: And Then There Were None
+ *           AUTHOR: Agatha Christie
+ *           NUMBER_OF_LOANS: 4
+ *           NUMBER_OF_FINES: 0
+ *         - TITLE: The Running Grave
+ *           AUTHOR: J. K. Rowling
+ *           NUMBER_OF_LOANS: 5
+ *           NUMBER_OF_FINES: 0
+ *         - TITLE: The Lord of the Rings
+ *           AUTHOR: John Ronald Reuel Tolkien
+ *           NUMBER_OF_LOANS: 5
+ *           NUMBER_OF_FINES: 2
+ */
+
+/**
+ * @swagger
  * tags:
  *   name: Chief Librarian
  *   description: Queries for the Chief Librarian
- * /PopularBooksByMonth:
+ * /PopularBooksByMonth/{course_id}/{year}/{timeframe}/{value}/{fetchnum}:
  *   get:
  *     security:
  *       - Bearer: []
- *     summary: Lists the most popular books in a selected timeframe for each course.
+ *     summary: Lists the most popular books in a selected timeframe for the selected course.
  *     tags: [Chief Librarian]
  *     parameters:
+ *       - name: course_id
+ *         in: path
+ *         description: Course to query.
+ *         required: true
+ *         schema:
+ *           type: integer
+ *           format: int32
  *       - name: year
- *         in: query
- *         description: The year for which to retrieve data.
+ *         in: path
+ *         description: Year to query.
  *         required: true
  *         schema:
  *           type: integer
  *           format: int32
- *           minimum: 2018
- *           maximum: 2024
- *       - name: month
- *         in: query
- *         description: The month (1 to 12) for which to retrieve data.
+ *       - name: timeframe
+ *         in: path
+ *         description: Timeframe to query.
+ *         required: true
+ *         schema:
+ *           type: string
+ *           enum: [Year, Quarter, Month, Week]
+ *       - name: value
+ *         in: path
+ *         description: The value of the timeframe (If selecting "Year", the values must match). E.G. 2023 for Year, 2 for Quarter, June for Month, 24 for Week.
+ *         required: true
+ *         schema:
+ *           type: string
+ *       - name: fetchnum
+ *         in: path
+ *         description: The number of departments to fetch (Top 3 departments, Top 5 departments etc).
  *         required: true
  *         schema:
  *           type: integer
  *           format: int32
- *           minimum: 1
- *           maximum: 12
+ *           minimum: 2
+ *           maximum: 6
  *     responses:
  *       200:
  *         description: List of books.
  *         content:
  *           application/json:
  *             schema:
- *               type: array
+ *               $ref: '#/components/schemas/PopularBooksByMonthResponse'
  *       403:
  *         description: Unauthorised. User does not have permissions to call this query.
  *       401:
