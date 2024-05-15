841fedb9b9bde77d2b2fd5b00e155124342bc382
Dylan Warrell <124311289+DylanW01@users.noreply.github.com>
2024-05-15 17:23:51 +0100

    Add table styling to queries

--- Client/src/app/pages/decision-maker-queries/finance-director/finance-director.component.ts
+++ Client/src/app/pages/decision-maker-queries/finance-director/finance-director.component.ts
@@ -1,8 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-finance-director',
-  templateUrl: './finance-director.component.html',
+  selector: "app-finance-director",
+  templateUrl: "./finance-director.component.html",
+  styleUrls: ["./finance-director.component.scss"],
 })
 export class AppFinanceDirectorComponent implements OnInit {
   constructor() {}
