4a1b8ef049509a6383159c0467661278b82260e8
Dylan Warrell <124311289+DylanW01@users.noreply.github.com>
2024-05-15 19:00:42 +0100

    404 Page & remove unused pages

--- Client/src/app/app-routing.module.ts
+++ Client/src/app/app-routing.module.ts
@@ -1,51 +1,42 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
-import { BlankComponent } from './layouts/blank/blank.component';
-import { FullComponent } from './layouts/full/full.component';
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
+import { BlankComponent } from "./layouts/blank/blank.component";
+import { FullComponent } from "./layouts/full/full.component";
+import { NotFoundComponent } from "./pages/not-found/not-found.component";
 
 const routes: Routes = [
   {
-    path: '',
+    path: "",
     component: FullComponent,
     children: [
       {
-        path: '',
-        redirectTo: '/dashboard',
-        pathMatch: 'full',
+        path: "",
+        redirectTo: "/dashboard",
+        pathMatch: "full",
       },
       {
-        path: 'dashboard',
-        loadChildren: () =>
-          import('./pages/pages.module').then((m) => m.PagesModule),
+        path: "dashboard",
+        loadChildren: () => import("./pages/pages.module").then((m) => m.PagesModule),
       },
       {
-        path: 'ui-components',
-        loadChildren: () =>
-          import('./pages/ui-components/ui-components.module').then(
-            (m) => m.UicomponentsModule
-          ),
-      },
-      {
-        path: 'decision-makers',
-        loadChildren: () =>
-          import(
-            './pages/decision-maker-queries/decision-maker-queries.module'
-          ).then((m) => m.DecisionMakerQueriesModule),
+        path: "decision-makers",
+        loadChildren: () => import("./pages/decision-maker-queries/decision-maker-queries.module").then((m) => m.DecisionMakerQueriesModule),
       },
     ],
   },
   {
-    path: '',
+    path: "",
     component: BlankComponent,
     children: [
       {
-        path: 'authentication',
-        loadChildren: () =>
-          import('./pages/authentication/authentication.module').then(
-            (m) => m.AuthenticationModule
-          ),
+        path: "authentication",
+        loadChildren: () => import("./pages/authentication/authentication.module").then((m) => m.AuthenticationModule),
       },
     ],
+  },
+  {
+    path: "**",
+    component: NotFoundComponent,
   },
 ];
 
