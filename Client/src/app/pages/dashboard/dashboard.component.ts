da2981857aa80b68eee4133ca7d7f0bc8f2aa115
Dylan Warrell <124311289+DylanW01@users.noreply.github.com>
2024-09-13 21:33:16 +0100

    Additional charts for dashboard

--- Client/src/app/pages/dashboard/dashboard.component.ts
+++ Client/src/app/pages/dashboard/dashboard.component.ts
@@ -1,4 +1,4 @@
-import { Component, ViewEncapsulation, ViewChild } from "@angular/core";
+import { Component, ViewEncapsulation, ViewChild, OnInit } from "@angular/core";
 import { ApexChart, ChartComponent, ApexDataLabels, ApexLegend, ApexStroke, ApexTooltip, ApexAxisChartSeries, ApexXAxis, ApexYAxis, ApexGrid, ApexPlotOptions, ApexFill, ApexMarkers, ApexResponsive } from "ng-apexcharts";
 import { DataAccessService } from "src/app/services/data-access.service";
 
@@ -49,7 +49,7 @@ export interface monthlyChart {
   templateUrl: "./dashboard.component.html",
   encapsulation: ViewEncapsulation.None,
 })
-export class AppDashboardComponent {
+export class AppDashboardComponent implements OnInit {
   @ViewChild("chart") chart: ChartComponent = Object.create(null);
 
   public loanFineOverviewChart!: Partial<loanFineOverviewChart> | any;
@@ -59,23 +59,17 @@ export class AppDashboardComponent {
   loansAndFines: any[] = [];
   fineIncome: any[] = [];
   quarterlyFineIncome: any[] = [];
+  finesTotalThisMonth: number = 0;
+  finesChangePercentage: string = "";
+  fineChangeDirection: string = "";
+  currentQuarter: string = "";
+  lastQuarter: string = "";
+  fineQuarterChangeDirection: string = "";
+  finesQuarterChangePercentage: string = "";
+  fineQuarterIncome: number = 0;
 
   constructor(private api: DataAccessService) {
-    this.api
-      .getDashboardSummary()
-      .then((response: any) => {
-        this.loansAndFines = response.loansAndFines.reverse();
-        this.fineIncome = response.fineIncome;
-        this.quarterlyFineIncome = response.quarterlyFineIncome;
-        this.updateCharts();
-      })
-      .catch((error) => {
-        console.error("Error fetching data:", error);
-        console.log("An error occurred. Please try again later.");
-        alert(error.error.message);
-      });
-
-    // Update the charts with the new data
+    //#region Update the charts with the new data
     this.loanFineOverviewChart = {
       series: [],
       grid: {
@@ -141,10 +135,11 @@ export class AppDashboardComponent {
         },
       ],
     };
+    //#endregion
 
-    // yearly breakup chart
+    //#region quarterly breakup chart
     this.yearlyChart = {
-      series: [38, 40, 25],
+      series: [],
 
       chart: {
         type: "donut",
@@ -155,7 +150,7 @@ export class AppDashboardComponent {
         },
         height: 130,
       },
-      colors: ["#5D87FF", "#ECF2FF", "#F9F9FD"],
+      colors: ["#5D87FF", "#ECF2FF"],
       plotOptions: {
         pie: {
           startAngle: 0,
@@ -189,14 +184,15 @@ export class AppDashboardComponent {
         enabled: false,
       },
     };
+    //#endregion
 
-    // mohtly earnings chart
+    //#region mohtly earnings chart
     this.monthlyChart = {
       series: [
         {
           name: "",
           color: "#49BEFF",
-          data: [25, 66, 20, 40, 12, 58, 20],
+          data: [],
         },
       ],
 
@@ -232,6 +228,23 @@ export class AppDashboardComponent {
         },
       },
     };
+    //#endregion
+  }
+
+  ngOnInit(): void {
+    this.api
+      .getDashboardSummary()
+      .then((response: any) => {
+        this.loansAndFines = response.loansAndFines.reverse();
+        this.fineIncome = response.fineIncome.reverse();
+        this.quarterlyFineIncome = response.quarterlyFineIncome.reverse();
+        this.updateCharts();
+      })
+      .catch((error) => {
+        console.error("Error fetching data:", error);
+        console.log("An error occurred. Please try again later.");
+        alert(error.error.message);
+      });
   }
 
   updateCharts() {
@@ -251,16 +264,31 @@ export class AppDashboardComponent {
 
     this.loanFineOverviewChart = { ...this.loanFineOverviewChart, xaxis: { type: "datetime", categories: this.loansAndFines.map((item) => item.MONTH) } };
 
-    // Update the 'yearlyChart' data
-    this.yearlyChart.series = this.quarterlyFineIncome.map((item) => item.TOTALFINEINCOME);
+    // Update the 'quarterlyChart' data
+    this.yearlyChart.series = this.quarterlyFineIncome.map((item) => item.TOTALFINEINCOME).reverse();
+    let quarterlyFineIncomeLength = this.quarterlyFineIncome.length;
+    this.currentQuarter = "Q" + this.quarterlyFineIncome[quarterlyFineIncomeLength - 1].QUARTER + "-" + String(this.quarterlyFineIncome[quarterlyFineIncomeLength - 1].YEAR).slice(-2);
+    this.lastQuarter = "Q" + this.quarterlyFineIncome[quarterlyFineIncomeLength - 2].QUARTER + "-" + String(this.quarterlyFineIncome[quarterlyFineIncomeLength - 2].YEAR).slice(-2);
+    this.fineQuarterIncome = this.quarterlyFineIncome[quarterlyFineIncomeLength - 1].TOTALFINEINCOME;
+    let finesTotalThisQuarter = this.quarterlyFineIncome[quarterlyFineIncomeLength - 1].TOTALFINEINCOME;
+    let finesTotalLastQuarter = this.quarterlyFineIncome[quarterlyFineIncomeLength - 2].TOTALFINEINCOME;
+    let finesQuarterChange = ((finesTotalThisQuarter - finesTotalLastQuarter) / finesTotalLastQuarter) * 100;
+    this.fineQuarterChangeDirection = finesQuarterChange >= 0 ? "up" : "down";
+    this.finesQuarterChangePercentage = (finesQuarterChange >= 0 ? "+" : "") + finesQuarterChange.toFixed(0) + "%";
 
     // Update the 'monthlyChart' data
     this.monthlyChart.series = [
       {
-        name: "",
+        name: "£",
         color: "#49BEFF",
         data: this.fineIncome.map((item) => item.TOTALFINEINCOME),
       },
     ];
+    let fineIncomeLength = this.fineIncome.length;
+    this.finesTotalThisMonth = this.fineIncome[fineIncomeLength - 1].TOTALFINEINCOME;
+    let finesTotalLastMonth = this.fineIncome[fineIncomeLength - 2].TOTALFINEINCOME;
+    let finesChange = ((this.finesTotalThisMonth - finesTotalLastMonth) / finesTotalLastMonth) * 100;
+    this.fineChangeDirection = finesChange >= 0 ? "up" : "down";
+    this.finesChangePercentage = (finesChange >= 0 ? "+" : "") + finesChange.toFixed(0) + "%";
   }
 }
