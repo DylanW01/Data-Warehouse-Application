13a69b1dce5de3cd21b8a2d6ffd760da8c219516
Dylan Warrell <124311289+DylanW01@users.noreply.github.com>
2024-09-13 21:30:40 +0100

    Working user accounts & authentication

---
+++ Server/src/swaggerConfig/7. Authentication.js
@@ -0,0 +1,11 @@
+/**
+ * @swagger
+ * components:
+ *   securitySchemes:
+ *     Bearer:
+ *       type: apiKey
+ *       name: Authorization
+ *       in: header
+ *       description: >- 
+ *        Enter the token from the `login` endpoint with the `Bearer: ` prefix, e.g. "Bearer abcde12345".
+ */
